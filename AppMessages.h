ae91a83cefc87e73308bfa9d0849c1d5efb4185a
Chris Keller <ckeller@punchthrough.com>
2014-06-11 15:05:54 -0700

    Added MIT license info.

--- AppMessages.h
+++ AppMessages.h
@@ -7,10 +7,25 @@
 
   Reference: @ref APP_MESSAGES <BR><BR>
 
-  Copyright 2014 Punch Through Design, LLC. All rights reserved.
+  Copyright (C) 2014 Punch Through Design, LLC.
 
-  Should you have any questions regarding your right to use this Software,
-  contact Punch Through Design at contact@punchthrough.com
+  Permission is hereby granted, free of charge, to any person obtaining a copy 
+  of this software and associated documentation files (the "Software"), 
+  to deal in the Software without restriction, including without limitation the 
+  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+  sell copies of the Software, and to permit persons to whom the Software is 
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all 
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+  SOFTWARE.
 */
 
 
